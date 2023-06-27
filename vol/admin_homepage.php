460036d3d594399e5ff7132fc270970c73d8e104
imenhajjaj <96117949+imenhajjaj@users.noreply.github.com>
2023-06-27 11:54:11 +0100

    Add files via upload

---
+++ vol/admin_homepage.php
@@ -0,0 +1,49 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>
+			Welcome Administrator
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<h1 id="title">
+			
+		</h1>
+		<div>
+			<ul>
+				<li><a href="admin_homepage.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+				<li><a href="admin_homepage.php"><i class="fa fa-desktop" aria-hidden="true"></i> Dashboard</a></li>
+				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+			</ul>
+		</div>
+		<h2>Welcome Administrator!</h2>
+		<table cellpadding="5">
+			
+			<tr>
+				<td class="admin_func"><a href="admin_view_booked_tickets.php"><i class="fa fa-plane" aria-hidden="true"></i> 
+Afficher la liste des billets réservés pour un vol</a>
+				</td>
+			</tr>
+			<tr>
+				<td class="admin_func"><a href="add_flight_details.php"><i class="fa fa-plane" aria-hidden="true"></i> Ajouter des détails sur les horaires de vol</a>
+				</td>
+			</tr>
+			<!-- <tr>
+				<td class="admin_func"><a href="modify_flight_details.php"><i class="fa fa-plane" aria-hidden="true"></i> Modify Flight Schedule Details</a>
+				</td>
+			</tr> -->
+			<tr>
+				<td class="admin_func"><a href="delete_flight_details.php"><i class="fa fa-plane" aria-hidden="true"></i> Supprimer les détails des horaires de vol
+</a>
+				</td>
+			</tr>
+	
+
+		
+		</table>
+	</body>
+</html>
---
+++ vol/home_page.php
@@ -0,0 +1,60 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>
+		    
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<h1 id="title">
+		</h1>
+		<div>
+			<ul>
+				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+				<li>
+					<?php
+						if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Customer')
+						{
+							echo "<a href=\"book_tickets.php\"><i class=\"fa fa-ticket\" aria-hidden=\"true\"></i> Book Tickets</a>";
+						}
+						else if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Administrator')
+						{
+							echo "<a href=\"admin_ticket_message.php\"><i class=\"fa fa-ticket\" aria-hidden=\"true\"></i> Book Tickets</a>";
+						}
+						else
+						{
+							echo "<a href=\"login_page.php\"><i class=\"fa fa-ticket\" aria-hidden=\"true\"></i> Book Tickets</a>";
+						}
+					?>
+				</li>				<li>
+					<?php
+						if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Customer')
+						{
+							echo "<a href=\"customer_homepage.php\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i> Login</a>";
+						}
+						else if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Administrator')
+						{
+							echo "<a href=\"admin_homepage.php\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i> Login</a>";
+						}
+						else
+						{
+							echo "<a href=\"login_page.php\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i> Login</a>";
+						}
+					?>
+				</li>
+			</ul>
+		</div>
+		<div class="container">
+			<div class="welcome_text"></div>
+			<img src="images/vol2.jfif" width=100%>
+		</div>
+		<!--check out addling local host in links and other places
+
+			shift login/logout buttons to right side
+		-->
+	</body>
+</html>
